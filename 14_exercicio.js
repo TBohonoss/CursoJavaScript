9d311d5e5e3afcb86a91b89129d8b38d4a896219
Thiago <thiago.bohonos@gmail.com>
2022-11-25 18:17:31 -0300

    Inserindo um novo array

--- 14_exercicio.js
+++ 14_exercicio.js
@@ -1,6 +1,6 @@
 // Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado.
 
-const numeros = [1,2,3,4,5,6,7,8,9,10]
+const numeros = [7,5,8,9,4,3,10,14]
 
 
 for (let i = 0; i < numeros.length; i++) {
